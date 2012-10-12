5130cbeeb9bb7c6cd7909da6de1384c3eb624d9b
Geoff Wagstaff <geoffwagstaff@gmail.com>
2012-10-12 17:10:03 +0100

    Worker for processing jobs. Includes tests

--- test/queue.js
+++ test/queue.js
@@ -35,10 +35,53 @@ describe('Enqueing jobs', function(done){
     var q = Convoy.createQueue('q');
     var job = new Convoy.Job(1);
     q.addJob(job, function(){
-      client.sismember(helpers.key('queued'), job.id, function(err, isMember){
+      client.lrange(helpers.key(q.name+':queued'), 0, -1, function(err, list){
         should.not.exist(err);
-        should.exist(isMember);
-        isMember.should.equal(1);
+        list.should.include(''+job.id);
+        done();
+      });
+    });
+  });
+});
+
+describe('Processing jobs', function(){
+  var q, job, processed;
+  before(function(done){
+    q = Convoy.createQueue('q');
+    q.process(function(j, p){
+      job = j, processed = p;
+      done();
+    });
+  });
+
+  it('invokes callback with job', function(done){
+    job.id.should.equal('1');
+    done();
+  });
+
+  it('removes job from queued list', function(done){
+    client.lrange(helpers.key(q.name+':queued'), 0, -1, function(err, list){
+      should.not.exist(err);
+      list.should.not.include(''+job.id);
+      done();
+    });
+  });
+
+  it('places job in processing list with timestamp', function(done){
+    client.zscore(helpers.key(q.name+':processing'), job.id, function(err, timestamp){
+      should.not.exist(err);
+      should.exist(timestamp);
+      timestamp.should.be.within(helpers.time() - 5, helpers.time());
+      done();
+    });
+  });
+
+  it('places job in fail list if callback invoked with error', function(done){
+    processed('ohnoes, something went wrong', function(){
+      client.zscore(helpers.key(q.name+':failed'), job.id, function(err, numFails){
+        should.not.exist(err);
+        should.exist(numFails);
+        numFails.should.equal(''+job.id);
         done();
       });
     });
