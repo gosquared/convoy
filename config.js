7e40db2c54603962f9269b5edad5b4ab686beda9
Geoff Wagstaff <geoffwagstaff@gmail.com>
2012-10-25 09:37:34 +0100

    Export production defaults if not testing module directly

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 var debug = require('debug')('convoy:config');
 
-// Check for config module. If not existent, export an empty config
 try{
   var config = require('config');
   debug('Loading config from node-config');
-  module.exports = config.setModuleDefaults('convoy', require('./config/default'));
+  module.exports = config.setModuleDefaults('convoy', process.env.NODE_ENV == 'test' ? config : require('./config/default'));
 }
 catch(e){
-  debug('Setting config to empty object');
-  module.exports = {};
+  debug('Setting config to production defaults');
+  module.exports = require('./config/default');
 }
-
